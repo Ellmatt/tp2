90ee86090cbba19b43e29c1ce4160afede7dd4e2
matias <pagosptcmatias@gmail.com>
2022-07-12 19:43:11 -0300

    ag ej 10

--- js/ejercicios/ejercicio10-tp2.js
+++ js/ejercicios/ejercicio10-tp2.js
@@ -4,18 +4,20 @@
 
 // en cada celda debe tener un numero descendente de multiplicar filas por columnas
 
-let filas = parseInt(prompt('ingrese un numero de filas'))
-//let columnas = parseInt(prompt('ingrese un numero de columnas'))
+let filas = parseInt(prompt("ingrese un numero de filas"));
+let columnas = parseInt(prompt("ingrese un numero de columnas"));
+let resultado = filas * columnas;
 
-document.write( '<table> <tbody>')
+document.write("<table> <tbody>");
 
 //este se encarga de dibujar las filas
-for(let indicefila = 0; indicefila < filas; indicefila++){
-document.write(`
-            <tr>
-<td>pueba</td>
-<td>datos</td>
-            </tr>
-        `);}
+for (let indicefila = 0; indicefila < filas; indicefila++) {
+  document.write("<tr>");
+  for (let indicecolumna = 0; indicecolumna < columnas; indicecolumna++){
+    document.write(`<td>${resultado}</td>`); // document.write(`<td>` + resultado + `</td>`)
+    resultado--;
+  }
+  document.write("</tr>");
+}
 
-        document.write( '</table> </tbody>')
+document.write("</table> </tbody>")
