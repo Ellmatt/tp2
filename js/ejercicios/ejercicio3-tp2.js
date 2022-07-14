1060c4bf511eba4fd6d65adc24a04c95ad71ca29
matias <pagosptcmatias@gmail.com>
2022-07-14 19:49:17 -0300

    correccion ej 3

--- js/ejercicios/ejercicio3-tp2.js
+++ js/ejercicios/ejercicio3-tp2.js
@@ -11,46 +11,8 @@ Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp */
     do{
         let texto = prompt("Ingrese un texto de hasta 10 caracteres, tener en cuenta que un espacio cuenta como caracter");
   console.log(texto);
-        let resultado = texto % 2;
-    console.log(resultado)
-    if (texto<=10 && texto >= 0) {
-    
-        switch (resultado) {
-          case 1:
-            document.write(texto );
-            
-            break;
-          case 2:
-            document.write(texto);
-            break;
-          case 3:
-            document.write(texto);
-            break;
-          case 4:
-            document.write(texto);
-            break;
-          case 5:
-            document.write(texto);
-            break;
-          case 6:
-            document.write(texto);
-            break;
-          case 7:
-            document.write(texto);
-            break;
-          case 8:
-            document.write(texto);
-            break;
-          case 9:
-            document.write(texto);
-            break;
-          case 10:
-            document.write(texto);
-            break;
-         
-          default:
-            alert("ingresaste una opcion erronea");
-        }
+  if (texto = true) {
+    document.write('-'+ texto)
       } 
       
     } while (confirm("desea ingresar un texto?")); 
