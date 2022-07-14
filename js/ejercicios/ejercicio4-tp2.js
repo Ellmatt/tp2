f07b3bee4b2eb808d841a25817a76b0851c98729
matias <pagosptcmatias@gmail.com>
2022-07-14 20:15:20 -0300

    agg ej 4

--- js/ejercicios/ejercicio4-tp2.js
+++ js/ejercicios/ejercicio4-tp2.js
@@ -2,5 +2,14 @@
 Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. 
 Al salir con “cancelar” deberá indicarse la suma total de los números introducidos. */
 do {
+  let numero1 = parseInt(prompt("ingrese un numero para sumar"));
   
+  
+  
+  if (numero1 >= 0 ){
+    
+    document.write((numero1 + numero1))
+  } else {
+    alert('la opcion ingresada no es un numero')
+  }
 } while (confirm("desea ingresar un numero?"));
