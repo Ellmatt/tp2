365f575c2bca583c199a53fc9cdcb95d7265e6c6
matias <pagosptcmatias@gmail.com>
2022-07-14 21:50:30 -0300

    agg ej 15 y 12

---
+++ js/ejercicios/ejercicio12-tp2.js
@@ -0,0 +1,6 @@
+/*12- Realiza un script que genere un número aleatorio entre 1 y 99 */
+let inicio = 1;
+let fin = 99;
+
+let enteroaleatorio= inicio + Math.floor(Math.random() * fin);
+ console.log(enteroaleatorio)
